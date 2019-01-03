3cd43f7c610af7c60853bcba730022412d251fe4
Jesse Mazzie <jessegalt@yahoo.com>
2019-01-03 18:04:07 -0500

    Added OS checking, modified ping command based on OS.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import subprocess as sp
+import os
+
+isWindows = os.name == "nt"
 
 def ping(host):
 	#TODO: Check for OS, change ping_string accordingly
-	ping_string = 'ping -n 1 ' + host
+	if isWindows:
+		ping_string = 'ping -n 1 ' + host
+	else:
+		ping_string = 'ping ' + host
 	
 	return sp.call(ping_string, False) == 0
 
