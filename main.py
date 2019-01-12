55753d9cd48135bc61167a3e4a9ace2438ebef83
Jesse Mazzie <jessegalt@yahoo.com>
2019-01-12 18:37:18 -0500

    Added Flask webserver to convert project into a REST API.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask
 import network_utils as nu
 
+app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Hello, world!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 #print(nu.checkport("www.google.com", 65546))
 #print(ping("www.google.com"))
