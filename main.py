3371ff148ea90039195294c7790e5a91624e2a2d
Jesse Mazzie <jessegalt@yahoo.com>
2019-01-12 18:32:32 -0500

    Moved network-related source code to network_utils.py

--- main.py
+++ main.py
@@ -1,38 +1,9 @@
-import subprocess as sp
-import logging
-import socket
-import os
-
-isWindows = os.name == "nt"
-if isWindows:
-	logging.info("This is a Windows system.")
-else:
-	logging.info("This is not a Windows system.")
+from flask import Flask
+import network_utils as nu
 
 
-def ping(host):
-	if isWindows:
-		ping_string = 'ping -n 1 ' + host
-	else:
-		ping_string = 'ping ' + host
-	
-	#Runs command returns a boolean indicating whether server responded to ping or not.
-	#Shell output is routed to OS' equivalent of /dev/null
-	with open(os.devnull, 'w') as devnull:
-		return sp.call(ping_string, stdout=devnull) == 0
 
-
-def checkport(host, portnum):
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	try:
-		result = sock.connect_ex((host, portnum))
-	except TypeError:
-		logging.error("portNum is expected to be an integer.")
-		
-	return result == 0
-
-
-print(checkport("www.google.com", 22))
+#print(nu.checkport("www.google.com", 65546))
 #print(ping("www.google.com"))
 
 
---
+++ network_utils.py
@@ -0,0 +1,35 @@
+import subprocess as sp
+import logging
+import socket
+import os
+
+isWindows = os.name == "nt"
+if isWindows:
+    logging.info("This is a Windows system.")
+else:
+    logging.info("This is not a Windows system.")
+
+
+def ping(host):
+    if isWindows:
+        ping_string = 'ping -n 1 ' + host
+    else:
+        ping_string = 'ping ' + host
+
+    # Runs command returns a boolean indicating whether server responded to ping or not.
+    # Shell output is routed to OS' equivalent of /dev/null
+    with open(os.devnull, 'w') as devnull:
+        return sp.call(ping_string, stdout=devnull) == 0
+
+
+def checkport(host, portnum):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        result = sock.connect_ex((host, portnum))
+    except TypeError:
+        logging.error("Port number is expected to be an integer.")
+    except OverflowError:
+        logging.error("Port number must be 0-65535.")
+
+    return result == 0
+
