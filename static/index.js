5d143dbfbf95fea30d7399c1ac665bfcdeffedf0
Jesse Mazzie <jessegalt@yahoo.com>
2019-05-01 18:40:11 -0400

    Added placeholder event listener for submit button.

--- static/index.js
+++ static/index.js
@@ -1,1 +1,10 @@
-//placeholder
+window.onload = init;
+
+//Various tasks performed on page load.
+function init() {
+    let submitBtn = document.getElementById("add-service-button");
+
+    submitBtn.addEventListener('click', function () {
+        alert('test');
+    });
+}
